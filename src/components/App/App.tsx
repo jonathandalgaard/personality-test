00bbff8da71986d79b75cc92ee7af567da6c6c19
Jonathan Dalgaard <jonathan@revealit.dk>
2023-05-07 11:31:06 +0200

    Initial dark mode implementation

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -4,18 +4,28 @@ import Quiz from 'components/pages/Quiz/Quiz';
 import './App.scss'
 import { Provider } from 'react-redux';
 import { store } from 'redux/store';
+import { useState } from 'react';
+import { MoonIcon, SunIcon } from 'lucide-react';
 
 function App() {
+  const [mode, setMode] = useState<'light' | 'dark'>('light');
+
   return (
     <Provider store={store}>
       <BrowserRouter>
-        <Routes>
-          <Route path="/" element={<LandingPage />} />
-          <Route
-            path="/quiz/:quizId"
-            element={<Quiz />}
-          />
-        </Routes>
+        <main className={mode}>
+          {mode === 'light'
+            ? <SunIcon className="mode-switcher" onClick={() => setMode('dark')} />
+            : <MoonIcon className="mode-switcher" onClick={() => setMode('light')} />
+          }
+          <Routes>
+            <Route path="/" element={<LandingPage />} />
+            <Route
+              path="/quiz/:quizId"
+              element={<Quiz />}
+            />
+          </Routes>
+        </main>
       </BrowserRouter>
     </Provider>
   )
